fef4ad7582117d0fd5629fb6bea06a82508d40f2
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-06-27 15:52:09 -0400

    roboshop code new

---
+++ function.sh
@@ -0,0 +1,12 @@
+# if we assign a name to a set of commands, then it is function
+# syntax function function_name(){
+# set of commands
+}
+
+# example
+sample() {
+  echo sample function
+
+  }
+sample
+

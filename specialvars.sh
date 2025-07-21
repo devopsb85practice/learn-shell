0e6939d9ddca29e56ab1cdfaded62562d6fb7df1
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-21 13:35:11 -0400

    practice

--- specialvars.sh
+++ specialvars.sh
@@ -3,3 +3,11 @@
 
 echo "all variables passed to the script : $@"
 echo " no of variables passed: $#"
+echo "script name: $0"
+echo "current dir: $PWD"
+echo "user running this script :$USER"
+echo "Home directory of user:$HOME"
+echo "PID of the script:$$"
+sleep 10 &
+echo "PID of last command in background:$!"
+ 

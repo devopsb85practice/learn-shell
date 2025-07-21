25988fea147b29046547f7f05ef7b13826a02111
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-21 11:06:50 -0400

    shell practice

--- variables.sh
+++ variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PERSON1 = trump
-PERSON2 = musk
+$PERSON1 = trump
+$PERSON2 = musk
 
 echo "$PERSON1: hey musk, how are you?"
-echo "$person2: hello $PERSON1, i am fine, how are you?"
-echo "$PERSON1: ok $person2"
+echo "$PERSON2: hello $PERSON1, i am fine, how are you?"
+echo "$PERSON1: ok $PERSON2"

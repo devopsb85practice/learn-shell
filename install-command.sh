fdeb34221f4050d73982c8dc630eadea974dfad6
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-21 14:33:20 -0400

    practice

--- install-command.sh
+++ install-command.sh
@@ -5,6 +5,7 @@ USERID=$(id -u)
 if [ "$USERID -nt 0" ]
 then
     echo "ERROR :: please run the script with root access"
+    exit 1 #other than 0
 else
     echo "you are running with root access"
 fi

2a8341eb054c7e9163f4de3875915b9569f7f35c
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-21 13:56:21 -0400

    practice

--- conditions.sh
+++ conditions.sh
@@ -3,8 +3,9 @@
 NUMBER=$1
 
 if [$NUMBER -lt 10]
-echo "number is less than 1o"
+then
+    echo "number is less than 1o"
 
 else 
-echo "number is not less than 10"
+    echo "number is not less than 10"
 fi

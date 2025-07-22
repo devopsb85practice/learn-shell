5516dde626f98a5baadf379f38be023778ea4fe3
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-21 21:20:57 -0400

    practice

--- functions.sh
+++ functions.sh
@@ -2,15 +2,6 @@
 
 USERID=$(id -u)
 
-VALIDATE(){
-if [ $1 -eq 0 ]
-    then
-        echo "installing $2 is success"
-    else
-        echo "installing $2 is failure"
-        exit 1
-    fi
-}
 
 if [ "$USERID" -ne 0 ]
 then
@@ -20,6 +11,15 @@ else
     echo "you are running with root access"
 fi
 dnf list installed mysql
+VALIDATE(){
+if [ $1 -eq 0 ]
+    then
+        echo "installing $2 is success"
+    else
+        echo "installing $2 is failure"
+    
+    fi
+}
 if [ $? -ne 0 ]
 then
     echo "Mysql not installed ...going to install now"
@@ -29,6 +29,7 @@ else
     echo " mysql already installed"
     exit 0
 fi
+
 dnf list installed python3
 if [ $? -ne 0 ]
 then

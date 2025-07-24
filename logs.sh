6f0ecead28db4919825ab6da0f8543fb94f16a89
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-23 21:24:00 -0400

    practice

--- logs.sh
+++ logs.sh
@@ -8,7 +8,7 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME="echo $0 | cut -d "." -f1"
 LOG_FILE="/$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -P $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
 echo "script started executing at $(date)"
 
 if [ "$USERID" -ne 0 ]

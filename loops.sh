91041066adf5eec034e9886e8e6196d783ec7138
lakshiprasannavelaga <prasanna.v373@gmail.com>
2025-07-24 09:41:33 -0400

    practice

--- loops.sh
+++ loops.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-for i in {1..100}
-do 
-    echo "$i"
-done
+# for i in {1..100}
+# do 
+#     echo "$i"
+# done
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="/$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+VALIDATE(){
+if [ $1 -eq 0 ]
+    then
+        echo -e "$N installing $2 is $G success $N" | tee -a $LOG_FILE
+    else
+        echo "$N installing $2 is $R failure $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "script started executing at $(date)"| tee -a $LOG_FILE
+
+PACKAGES=("nginx" "mysql", "python3")
+for package in "{$PACKAGES[@]}"
+do
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+then
+    echo -e "$N $package not installed ...going to install now $N" &>>$LOG_FILE
+    dnf install $package -y | tee -a $LOG_FILE
+    VALIDATE $? "$package"
+else
+    echo -e "$Y $pakage already installed $N" | tee -a $LOG_FILE
+fi
+  
+
+
